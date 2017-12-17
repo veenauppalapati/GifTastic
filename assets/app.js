b7baaa1c01f08441036b43769ec6602f97cdb58f
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2017-12-17 00:11:15 -0500

    gifs-added

--- assets/app.js
+++ assets/app.js
@@ -5,23 +5,27 @@ $("document").ready(function(){
   var animals=[];
   console.log(animals);
 //================================================
+//===============FUNCTIONS ===================//
 
 function renderButtons(){
 
+$("#buttons-view").empty();
   /*------------------------------------------*/
   for (var i = 0; i < animals.length; i++){
-    $(".buttonsection").empty();
+     
     // we created a button tag
     var button = $("<button>");
     button.addClass("userButtons");
     button.attr("data-name", animals[i]);
     var buttonName = button.text(animals[i]); 
      $("#buttons-view").append(buttonName);
+
   }
+
   /*------------------------------------------*/
 }
-renderButtons();
 
+//============= END OF FUNCTIONS ==================
 //================================================
 //===============EVENTLISTENER ===================//
   
@@ -32,18 +36,49 @@ renderButtons();
   animals.push(animal);
   renderButtons();
 
+  $("#userinput").val("");
 /* ---------------------------------------------*/
-
+    
   $(".userButtons").on("click", function(){
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        animal + "&api_key=9kmoXaUOhNIXuAzBROxg37xQmuBP2X01&limit=10";
 
-  alert("I'm working");
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      console.log(queryURL);
+      console.log(response);
 
-  });
+      var results = (response.data);
+       // Looping through each result item
+          for (var i = 0; i < results.length; i++) {
+
+            // Creating and storing a div tag
+            var animalDiv = $("<div>");
+
+            // Creating and storing an image tag
+            var animalImage = $("<img>");
+            // Setting the src attribute of the image to a property pulled off the result item
+            animalImage.attr("src", results[i].images.fixed_height.url);
+
+             // Appending the paragraph and image tag to the animalDiv
+            
+            animalDiv.append(animalImage);
+            $("#gifssection").prepend(animalDiv);
+          }
+
+
+    });
+
+  });// CLOSING THE EVENT USERBUTTONS
 
 //================================================
 
- }); // CLOSING THE USER
+ }); // CLOSING THE EVENT SUBMIT BUTTON
 
+//============== END OF EVENTLISTENER ============
 //================================================
 
 });// CLOSING THE READY FUNCTION
