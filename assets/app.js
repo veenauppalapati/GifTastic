48c18a828bd8de4928bc1490ff3a8739e8c8defa
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2017-12-18 13:00:23 -0500

    pausing gifs

--- assets/app.js
+++ assets/app.js
@@ -24,7 +24,7 @@ $("#buttons-view").empty();
 
   /*------------------------------------------*/
 }
-
+//================================================
 //============= END OF FUNCTIONS ==================
 //================================================
 //===============EVENTLISTENER ===================//
@@ -40,37 +40,52 @@ $("#buttons-view").empty();
 /* ---------------------------------------------*/
     
   $(".userButtons").on("click", function(){
-    
+    // This is our API key
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=9kmoXaUOhNIXuAzBROxg37xQmuBP2X01&limit=10";
-
+    //  Here we are building the URL we need to query the database
     $.ajax({
       url: queryURL,
       method: "GET"
     }).done(function(response) {
       console.log(queryURL);
       console.log(response);
-
+      // variable to store the informationn from the data
       var results = (response.data);
-       // Looping through each result item
-          for (var i = 0; i < results.length; i++) {
+      console.log(results);
 
-            // Creating and storing a div tag
-            var animalDiv = $("<div>");
-
-            // Creating and storing an image tag
-            var animalImage = $("<img>");
+      for(var i = 0; i<results.length; i++){
+        
+         var animalImage = $("<img>");
             // Setting the src attribute of the image to a property pulled off the result item
-            animalImage.attr("src", results[i].images.fixed_height.url);
+            animalImage.addClass("gifs")
+           animalImage.attr("animate", results[i].images.original.url);
+           animalImage.attr("still", results[i].images.original_still.url);
+         animalImage.attr("src", results[i].images.original_still.url );
 
-             // Appending the paragraph and image tag to the animalDiv
-            
-            animalDiv.append(animalImage);
-            $("#gifssection").prepend(animalDiv);
+          animalImage.attr("datastate","still");
+            $("#gifssection").prepend(animalImage);
           }
+ 
+      $(".gifs").on("click", function() {
+        var state = $(this).attr("datastate");
+        if(state  === "still")
+        {
+          // alert("im working");
+          $(this).attr("src", $(this).attr("animate"));
+          $(this).attr("datastate", "animate")
+        }
+       else {
+         $(this).attr("src", $(this).attr("still"))
+         $(this).attr("datastate", "still")
 
-
+       }
     });
+          
+        
+    });
+
+       
 
   });// CLOSING THE EVENT USERBUTTONS
 
